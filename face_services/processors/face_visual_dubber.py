73b763bf88cd6384b4b6d622df99c88ad986f6a1
Thierry SAMMOUR SAWAYA <thierrysammour@gmail.com>
2023-10-25 14:06:52 +0200

    first commit

---
+++ face_services/processors/face_visual_dubber.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class VisualDubber:
+  
+	def __init__(self):
+		logging.info('VisualDubber - Initialize')
+
+	def run(self):
+		logging.info('VisualDubber - Run')
+		return None
+
